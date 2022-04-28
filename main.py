74661b213003d91a8422d7f5484bc399569fd37f
MichelleDardon10 <michelle.dardon@ufm.edu>
2022-04-27 20:19:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,22 @@ def print_board(board):
     print('\n'.join([str(row) for row in board]))
 
 
-def update_board(board, position, player):
-    # TODO: modificar tablero con nuevo movimiento del jugador en posicion indicada.
-    pass
+def update_board(board,player):
+    
+    for i in range(9):
+
+        position1 = int(input("Ingrese fila: "))
+        position2 = int(input("Ingrese columna: "))
+
+        player = int(input("1 for X/ 0 for O: "))
+
+        board[position1][position2] = player
+
+        print('\n'.join([str(row) for row in board]))
+
+        
+    
+
 
 
 def check_for_winner(board):
